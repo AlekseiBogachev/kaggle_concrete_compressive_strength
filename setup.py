757955340df848f8e881eda633ae1a4dd83345af
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-01-15 07:03:45 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example of a regression with Concrete Compressive Strength dataset from Kaggle',
+    author='Aleksei Bogachev',
+    license='MIT',
+)
